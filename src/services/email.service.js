5d774c5661b5eabb5fe37015f75841d343edae41
thucduy <thucduy0103@gmail.com>
2021-12-19 19:01:40 +0700

    message

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -33,10 +33,10 @@ const sendEmail = async (to, subject, text) => {
 const sendResetPasswordEmail = async (user, to, token) => {
   const subject = 'Reset password';
   // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
+  const resetPasswordUrl = `http://${config.frontEndApp.link}/reset-password?token=${token}`;
   const text = `Dear ${user},
-To reset your password, click on this link: ${resetPasswordUrl}
-If you did not request any password resets, then ignore this email.`;
+  To reset your password, click on this link: ${resetPasswordUrl}
+  If you did not request any password resets, then ignore this email.`;
   await sendEmail(to, subject, text);
 };
 
@@ -51,8 +51,8 @@ const sendVerificationEmail = async (to, token) => {
   // replace this url with the link to the email verification page of your front-end app
   const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
   const text = `Dear user,
-To verify your email, click on this link: ${verificationEmailUrl}
-If you did not create an account, then ignore this email.`;
+  To verify your email, click on this link: ${verificationEmailUrl}
+  If you did not create an account, then ignore this email.`;
   await sendEmail(to, subject, text);
 };
 
